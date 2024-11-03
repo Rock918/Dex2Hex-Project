748f166ed67a6932f5d222eec468d5f6eb041be1
“Rock918” <rimran300@caledonian.ac.uk>
2024-11-03 04:38:06 +0000

    fixed the code

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,26 +1,29 @@
 import java.util.Scanner;
 
-class Dec2Hex
-{
-public static int Arg1;
-    public static void main(String args[])    {
+public class Dex2Hex {
+    public static int Arg1;
+
+    public static void main(String args[]) {
+        // Validate input
+        if (args.length < 1) {
+            System.out.println("Please provide a decimal number as an argument.");
+            return;
+        }
+
         Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
         int rem, num;
         num = Arg1;
-        String hexadecimal="";
+        String hexadecimal = "";
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+        while (num != 0) {
+            rem = num % 16;
+            hexadecimal = ch[rem] + hexadecimal;
+            num = num / 16;
         }
 
         System.out.println("Hexadecimal representation is: " + hexadecimal);
-
     }
 }
 
-//a new change
