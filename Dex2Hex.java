491d29c6f4f10030075d07b794e5cd5616cb9c8b
“Rock918” <rimran300@caledonian.ac.uk>
2024-11-03 04:22:08 +0000

    code will run

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Dec2Hex
+public class Dec2Hex
 {
 public static int Arg1;
     public static void main(String args[])    {
@@ -19,6 +19,7 @@ public static int Arg1;
         }
 
         System.out.println("Hexadecimal representation is: " + hexadecimal);
+	System.out.println("Dex2Hex program is running");
 
     }
 }
